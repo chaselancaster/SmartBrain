d441c8f04e9c9c003498c03806c46010b95e0e39
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-27 16:13:22 -0700

    add brain to img src

--- src/Component/Logo/Logo.js
+++ src/Component/Logo/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import brain from './brain.png';
 import './Logo.css';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-            <div className="Tilt-inner"> 👽 </div>
+            <div className="Tilt-inner">
+                <img src={brain} alt='logo'></img>
+            </div>
             </Tilt>
         </div>
     )
