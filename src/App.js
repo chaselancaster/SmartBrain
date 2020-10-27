7b7c21fac729b50e01d1c92d65e5cf909dbcaabb
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-27 16:34:59 -0700

    add Particles component and variable holding all of the params outside of the class App

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import ImageLinkForm from './Component/ImageLinkForm/ImageLinkForm';
 import Rank from './Component/Rank/Rank';
 import './App.css';
 
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles 
+              params={particlesOptions}
+            />
         <Navigation />
         <Logo />
         <Rank />
