65fa29ef0f191a9893327809a85bc93c93fbc9e4
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-27 16:01:23 -0700

    add Logo.css

--- src/Component/Logo/Logo.js
+++ src/Component/Logo/Logo.js
@@ -4,7 +4,7 @@ import Tilt from 'react-tilt'
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
             <div className="Tilt-inner"> 👽 </div>
             </Tilt>
         </div>
