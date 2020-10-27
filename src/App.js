e25165262e7b4f075b92af15b226eaf509876d88
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-27 15:57:00 -0700

    add Logo.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Navigation from './Component/Navigation/Navigation'
-import Logo from './Component/Logo/Logo'
+import Navigation from './Component/Navigation/Navigation';
+import Logo from './Component/Logo/Logo';
 import './App.css';
 
 class App extends Component {
