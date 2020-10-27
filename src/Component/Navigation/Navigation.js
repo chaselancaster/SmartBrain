21279fb8318e30549bb5f166e89210d84f954e79
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-27 15:49:47 -0700

    delete () from App.js

--- src/Component/Navigation/Navigation.js
+++ src/Component/Navigation/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navigation = () => {
     return (
-        <nav style={{display: 'flex', justifyContent='flex-end'}}>
+        <nav style={{display: 'flex', justifyContent:'flex-end'}}>
             <p>Sign Out</p>
         </nav>
     )
