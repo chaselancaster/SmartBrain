11d5aa464bf369dbdc42b51b94ad1df2d4a1c3f4
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-27 16:17:39 -0700

    add input and button to ImageLinkForm

--- src/Component/ImageLinkForm/ImageLinkForm.js
+++ src/Component/ImageLinkForm/ImageLinkForm.js
@@ -6,6 +6,10 @@ const ImageLinkForm = () => {
             <p className='f3'>
                 {'This Magic Brain will detect faces in your pictures. Give it a try.'}
             </p>
+            <div>
+                <input type='text' />
+                <button>Detect</button>
+            </div>
         </div>
     )
 }
