a90bb8ceaa5b8259835a5cf4a2c8e193a7033783
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-28 17:05:09 -0700

    add onRouteChange function and pass it to Signin component

--- src/App.js
+++ src/App.js
@@ -66,6 +66,10 @@ class App extends Component {
       .catch(error => console.log(error));
   }
 
+  onRouteChange = () => {
+    this.setState({route: 'home'})
+  }
+
   render() {
     return (
       <div className="App">
@@ -74,7 +78,7 @@ class App extends Component {
         />
         <Navigation />
         { this.state.route === 'signin'
-          ? <Signin />
+          ? <Signin onRouteChange={this.onRouteChange}/>
           : <div>
               <Logo />
               <Rank />
