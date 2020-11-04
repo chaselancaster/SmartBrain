c30a83ebe0a45f86372dca010e03f20e4d6f9c24
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-04 11:11:16 -0800

    add this.onPasswordChange to onChange function of password input

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -40,7 +40,12 @@ class Signin extends React.Component {
                     </div>
                     <div className="mv3">
                     <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                    <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                    <input 
+                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" 
+                    name="password"  
+                    id="password"
+                    onChange={this.onPasswordChange}
+                    />
                     </div>
                 </fieldset>
                 <div className="">
