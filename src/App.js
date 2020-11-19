dd26c3f3ce96a4c087d4545fe9c269d2352b9e22
Chase Lancaster <chaseblancaster@gmail.com>
2020-11-19 14:21:01 -0800

    remove port nymber from image fetch call on onButtonSubmit

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://glacial-dusk-47272.herokuapp.com:3000/image', {
+          fetch('https://glacial-dusk-47272.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
